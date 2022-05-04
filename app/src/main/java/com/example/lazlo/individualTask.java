1524f6504eb50c523022a8fc10e5161d93852b2e
m0ckinjay <m0ckinjay@yandex.com>
2022-05-04 05:49:58 +0300

    setup individual task layout

--- app/src/main/java/com/example/lazlo/individualTask.java
+++ app/src/main/java/com/example/lazlo/individualTask.java
@@ -1,11 +1,15 @@
 package com.example.lazlo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SimpleAdapter;
@@ -14,26 +18,40 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lazlo.Sql.DBHelper;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 public class individualTask extends AppCompatActivity {
-  EditText Ttitle, Tdescription;
-  Button Btnsave, Btnshow;
+  TextInputEditText individualTaskTitle_TextInputEdit, individualTaskDescription_TextInputEdit,
+          individualTaskBills_TextInputEdit,individualTaskDeadline_TextInputEdit;
+  AutoCompleteTextView individualTaskCategory_TextInputEdit;
+  AppCompatButton Btnsave, Btnshow;
   DBHelper dbHelper;
   long currentId;
   Cursor cursor;
+  String selectedCategory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_task);
-        Ttitle = findViewById(R.id.individualTaskTitle_TextInputEdit);
-        Tdescription = findViewById(R.id.individualTaskDescription_TextInputEdit);
+        individualTaskTitle_TextInputEdit = findViewById(R.id.individualTaskTitle_TextInputEdit);
+        individualTaskDescription_TextInputEdit = findViewById(R.id.individualTaskDescription_TextInputEdit);
+        individualTaskCategory_TextInputEdit = findViewById(R.id.individualTaskCategory_TextInputEdit);
+        individualTaskBills_TextInputEdit = findViewById(R.id.individualTaskBills_TextInputEdit);
+        individualTaskDeadline_TextInputEdit = findViewById(R.id.individualTaskDeadline_TextInputEdit);
         Btnsave = findViewById(R.id.Btnsave);
         Btnshow = findViewById(R.id.Btnshow);
         dbHelper = new DBHelper(this);
+
+        //populate category dropdown
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(),R.array.categories, android.R.layout.simple_dropdown_item_1line);
+        individualTaskCategory_TextInputEdit.setAdapter(adapter);
+        individualTaskCategory_TextInputEdit.setOnItemClickListener((adapterView, view, i, l) -> selectedCategory = (String) adapterView.getItemAtPosition(i));
+
+
         currentId = this.getIntent().getLongExtra("my_id_extra",-1);
         if (currentId < 0){
             //do something as invalid id passed
@@ -56,7 +74,7 @@ public class individualTask extends AppCompatActivity {
             public void onClick(View view) {
 
                 try {
-                    dbHelper.update(currentId,null,Ttitle.getText().toString(),Tdescription.getText().toString());
+                    dbHelper.update(currentId,null,individualTaskTitle_TextInputEdit.getText().toString(),individualTaskDescription_TextInputEdit.getText().toString());
                     Toast.makeText(getApplicationContext(),"Task updated successfully", Toast.LENGTH_LONG).show();
 
                 }catch (Exception e){
@@ -80,9 +98,9 @@ public class individualTask extends AppCompatActivity {
         System.out.println("done...");
         if (cursor.moveToFirst()){
             System.out.println("Setting text...");
-            Ttitle.setText(cursor.getString(cursor.getColumnIndexOrThrow("TaskTitle")));
+            individualTaskTitle_TextInputEdit.setText(cursor.getString(cursor.getColumnIndexOrThrow("TaskTitle")));
             System.out.println("TaskTitle" + cursor.getString(cursor.getColumnIndexOrThrow("TaskTitle")));
-            Tdescription.setText(cursor.getString(cursor.getColumnIndexOrThrow("TaskDescription")));
+            individualTaskDescription_TextInputEdit.setText(cursor.getString(cursor.getColumnIndexOrThrow("TaskDescription")));
             System.out.println("TaskDescription" + cursor.getString(cursor.getColumnIndexOrThrow("TaskDescription")));
         }
         cursor.close();
