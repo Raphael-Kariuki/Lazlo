032acf3a4e2a84a4c543e5f993810f604470d62d
m0ckinjay <m0ckinjay@yandex.com>
2022-07-27 17:45:12 +0300

    Removed unused exports

--- app/src/main/java/com/example/lazlo/AddTasks.java
+++ app/src/main/java/com/example/lazlo/AddTasks.java
@@ -7,7 +7,6 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -20,12 +19,10 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
-import java.util.Date;
+
 
 public class AddTasks extends AppCompatActivity {
     TextInputEditText task_title,task_description,select_date,priceAutocompleteView,selectTime_AutocompleteView;
