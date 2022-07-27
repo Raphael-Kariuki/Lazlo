dcde511f9cfc24d437a01f7ae3046db913fcb772
m0ckinjay <m0ckinjay@yandex.com>
2022-07-27 12:50:50 +0300

    populated months selection on dashboard and added background to months

--- app/src/main/java/com/example/lazlo/Dashboard.java
+++ app/src/main/java/com/example/lazlo/Dashboard.java
@@ -3,12 +3,29 @@ package com.example.lazlo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 
 public class Dashboard extends AppCompatActivity {
-
+MaterialAutoCompleteTextView monthsSelectionDropDownOnDashBoard;
+String selectedMonth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        monthsSelectionDropDownOnDashBoard = findViewById(R.id.monthsSelectionDropDownOnDashBoard);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.Months, android.R.layout.simple_dropdown_item_1line);
+        monthsSelectionDropDownOnDashBoard.setAdapter(adapter);
+        monthsSelectionDropDownOnDashBoard.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedMonth = (String) adapterView.getItemAtPosition(i);
+            }
+        });
     }
 }
