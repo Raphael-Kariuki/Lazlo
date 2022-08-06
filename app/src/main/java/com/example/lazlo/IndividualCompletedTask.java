f03e93222757c5a89cd5dd3704f57ff00d0699e8
m0ckinjay <m0ckinjay@yandex.com>
2022-08-06 18:11:48 +0300

    Didn't do conversions, populated individual completed task

--- app/src/main/java/com/example/lazlo/IndividualCompletedTask.java
+++ app/src/main/java/com/example/lazlo/IndividualCompletedTask.java
@@ -16,11 +16,26 @@ public class IndividualCompletedTask extends AppCompatActivity {
     MaterialTextView completedTaskTitle,completedTaskCategory,completedTaskCreationDate, completedTaskDeadline,completedTaskStartDate,completedTaskCompletionDate,completedTaskPredictedDuration,completedTaskActualDuration,completedTaskPredictedSpending,completedTaskActualSpending,completedTaskDescription;
     DBHelper dbHelper;
     Double randTaskId;
-    String completedTaskTitle_str,completedTaskCategory_str, completedTaskPredictedSpending_str,completedTaskDeadline_str, completedTaskCreationDate_str, completedTaskStartDate_str, completedTaskCompletionDate_str,completedTaskActualDuration_str;
+    String completedTaskTitle_str,completedTaskDescription_str,completedTaskCategory_str, completedTaskPredictedSpending_str,completedTaskDeadline_str, completedTaskCreationDate_str, completedTaskStartDate_str, completedTaskCompletionDate_str,completedTaskActualDuration_str;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_completed_task);
+
+
+        completedTaskTitle = findViewById(R.id.completedTaskTitle);
+        completedTaskDescription = findViewById(R.id.completedTaskDescription);
+        completedTaskCategory = findViewById(R.id.completedTaskCategory);
+        completedTaskCreationDate = findViewById(R.id.completedTaskCreationDate);
+        completedTaskDeadline = findViewById(R.id.completedTaskDeadlineDate);
+        completedTaskStartDate = findViewById(R.id.completedTaskStartDate);
+        completedTaskCompletionDate = findViewById(R.id.completedTaskCompletionDate);
+        completedTaskPredictedDuration = findViewById(R.id.completedTaskPredictedDuration);
+        completedTaskActualDuration = findViewById(R.id.completedTaskActualDuration);
+        completedTaskPredictedSpending = findViewById(R.id.completedTaskPredictedSpending);
+        completedTaskActualSpending = findViewById(R.id.completedTaskActualSpending);
+        completedTaskDescription = findViewById(R.id.completedTaskDescription);
+
 
         dbHelper = new DBHelper(this);
 
@@ -28,6 +43,7 @@ public class IndividualCompletedTask extends AppCompatActivity {
         randUserId = Double.parseDouble(spf.getString("randomUserId", null));
 
         selectedTaskId = getIntent().getLongExtra("selectedTaskId",-1);
+        System.out.println("Current id:" + selectedTaskId + " "+ randUserId);
         if (selectedTaskId < 0){
             //do something as invalid id passed
             finish();
@@ -40,17 +56,6 @@ public class IndividualCompletedTask extends AppCompatActivity {
         }
 
 
-        completedTaskTitle = findViewById(R.id.completedTaskTitle);
-        completedTaskCategory = findViewById(R.id.completedTaskCategory);
-        completedTaskCreationDate = findViewById(R.id.completedTaskCreationDate);
-        completedTaskDeadline = findViewById(R.id.completedTaskDeadlineDate);
-        completedTaskStartDate = findViewById(R.id.completedTaskStartDate);
-        completedTaskCompletionDate = findViewById(R.id.completedTaskCompletionDate);
-        completedTaskPredictedDuration = findViewById(R.id.completedTaskPredictedDuration);
-        completedTaskActualDuration = findViewById(R.id.completedTaskActualDuration);
-        completedTaskPredictedSpending = findViewById(R.id.completedTaskPredictedSpending);
-        completedTaskActualSpending = findViewById(R.id.completedTaskActualSpending);
-        completedTaskDescription = findViewById(R.id.completedTaskDescription);
 
 
     }
@@ -64,30 +69,34 @@ public class IndividualCompletedTask extends AppCompatActivity {
 
         if (cursor.moveToFirst()){
             randTaskId = cursor.getDouble(cursor.getColumnIndexOrThrow("completedTaskRandomId"));
+            System.out.println("Error: " + 0);
             completedTaskTitle_str = cursor.getString(cursor.getColumnIndexOrThrow("completedTaskTitle"));
+            System.out.println(completedTaskTitle_str + 0);
             completedTaskTitle.setText(completedTaskTitle_str);
-
+            System.out.println("Error: " + 1);
+            completedTaskDescription_str = cursor.getString(cursor.getColumnIndexOrThrow("completedTaskDescription"));
+            completedTaskDescription.setText(completedTaskDescription_str);
             completedTaskCategory_str = cursor.getString(cursor.getColumnIndexOrThrow("completedTaskCategory"));
             completedTaskCategory.setText(completedTaskCategory_str);
-
+            System.out.println("Error: " + 2);
             completedTaskPredictedSpending_str = cursor.getString(cursor.getColumnIndexOrThrow("completedTaskPredictedSpending"));
             completedTaskPredictedSpending.setText(completedTaskPredictedSpending_str);
-
+            System.out.println("Error: " + 3);
             completedTaskDeadline_str = cursor.getString(cursor.getColumnIndexOrThrow("completedTaskDeadline"));
             completedTaskDeadline.setText(completedTaskDeadline_str);
-
+            System.out.println("Error: " + 4);
             completedTaskCreationDate_str = cursor.getString(cursor.getColumnIndexOrThrow("completedTaskCreationDate"));
             completedTaskCreationDate.setText(completedTaskCreationDate_str);
-
+            System.out.println("Error: " + 5);
             completedTaskStartDate_str = cursor.getString(cursor.getColumnIndexOrThrow("completedTaskStartDate"));
             completedTaskStartDate.setText(completedTaskStartDate_str);
-
+            System.out.println("Error: " + 6);
             completedTaskCompletionDate_str = cursor.getString(cursor.getColumnIndexOrThrow("completedTaskCompletionDate"));
             completedTaskCompletionDate.setText(completedTaskCompletionDate_str);
-
+            System.out.println("Error: " + 7);
             completedTaskActualDuration_str = cursor.getString(cursor.getColumnIndexOrThrow("completedTaskActualDuration"));
             completedTaskActualDuration.setText(completedTaskActualDuration_str);
-
+            System.out.println("Error: " + 8);
         }else{
             System.out.println("Error populating view");
         }
