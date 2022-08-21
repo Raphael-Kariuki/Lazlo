f22b877bd2ba79532510ba6121eab002e7056dd0
m0ckinjay <m0ckinjay@yandex.com>
2022-08-21 14:44:02 +0300

    populate IndividualTask works

--- app/src/main/java/com/example/lazlo/taskModel.java
+++ app/src/main/java/com/example/lazlo/taskModel.java
@@ -1,15 +1,23 @@
 package com.example.lazlo;
 
 public class taskModel {
-    private String taskTitle;
-    private String taskDescription;
-    private String taskAssociatedPrice;
+
+    private String taskTitle,taskDescription, taskAssociatedPrice,
+            taskCategory,taskCreationTime,taskDeadline,taskPredictedDuration, taskState , parentTaskId;
+    private Double randTaskId,randUserId;
 
     //constructor
-    taskModel(String taskTitle, String taskDescription, String taskAssociatedPrice){
+    taskModel(String taskTitle, String taskDescription, String taskAssociatedPrice,String taskCategory,String taskCreationTime
+    ,String taskDeadline,String taskPredictedDuration,String taskState,String parentTaskId){
         this.taskTitle = taskTitle;
         this.taskDescription = taskDescription;
         this.taskAssociatedPrice = taskAssociatedPrice;
+        this.taskCategory = taskCategory;
+        this.taskCreationTime = taskCreationTime;
+        this.taskDeadline =taskDeadline;
+        this.taskPredictedDuration = taskPredictedDuration;
+        this.taskState = taskState;
+        this.parentTaskId = parentTaskId;
     }
     //getters and setters
 
@@ -25,6 +33,71 @@ public class taskModel {
     public String getTaskDescription() {
         return taskDescription;
     }
+
+    public String getTaskCategory() {
+        return taskCategory;
+    }
+
+    public Double getRandTaskId() {
+        return randTaskId;
+    }
+
+    public Double getRandUserId() {
+        return randUserId;
+    }
+
+    public String getParentTaskId() {
+        return parentTaskId;
+    }
+
+    public String getTaskCreationTime() {
+        return taskCreationTime;
+    }
+
+    public String getTaskPredictedDuration() {
+        return taskPredictedDuration;
+    }
+
+    public String getTaskDeadline() {
+        return taskDeadline;
+    }
+
+    public String getTaskState() {
+        return taskState;
+    }
+
+    public void setParentTaskId(String parentTaskId) {
+        this.parentTaskId = parentTaskId;
+    }
+
+    public void setTaskCategory(String taskCategory) {
+        this.taskCategory = taskCategory;
+    }
+
+    public void setRandTaskId(Double randTaskId) {
+        this.randTaskId = randTaskId;
+    }
+
+    public void setRandUserId(Double randUserId) {
+        this.randUserId = randUserId;
+    }
+
+    public void setTaskCreationTime(String taskCreationTime) {
+        this.taskCreationTime = taskCreationTime;
+    }
+
+    public void setTaskDeadline(String taskDeadline) {
+        this.taskDeadline = taskDeadline;
+    }
+
+    public void setTaskPredictedDuration(String taskPredictedDuration) {
+        this.taskPredictedDuration = taskPredictedDuration;
+    }
+
+    public void setTaskState(String taskState) {
+        this.taskState = taskState;
+    }
+
 
     public void setTaskTitle(String taskTitle) {
         this.taskTitle = taskTitle;
