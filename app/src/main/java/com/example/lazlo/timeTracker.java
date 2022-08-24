70f8f04a42b7834aee79c88cbd7b0ad118b67738
m0ckinjay <m0ckinjay@yandex.com>
2022-08-24 14:04:55 +0300

    timer working on pause, however on rotation buttons deactivate. Check on states

--- app/src/main/java/com/example/lazlo/timeTracker.java
+++ app/src/main/java/com/example/lazlo/timeTracker.java
@@ -1,16 +1,25 @@
 package com.example.lazlo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.lazlo.Sql.DBHelper;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textview.MaterialTextView;
 
@@ -20,7 +29,7 @@ import java.util.Date;
 public class timeTracker extends AppCompatActivity {
 MaterialButton startTimer, pauseTimer, resumeTimer, completeTimer;
 long startTimerTime, pauseTimerTime, resumeTimerTime, completeTimerTime, duration;
-MaterialTextView lostTime;
+MaterialTextView lostTime, previousLostTime;
 TextInputEditText lostTimeReasonTxt;
 String reason;
 ArrayList<timerModel> timerModelArrayList;
@@ -28,11 +37,163 @@ int state;
 DBHelper dbHelper;
 Double randUserId;
 SharedPreferences spf;
+private int seconds;
+private boolean running, wasRunning;
+    String stopWatch;
+
+    DrawerLayout timeTrackerDrawerLayout;
+    NavigationView timeTrackerNavigationView;
+    ActionBarDrawerToggle timeTrackerActionBarDrawerToggle;
+
+    //store page instance when page is paused or destroyed
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (!wasRunning){
+            wasRunning = running;
+            running = false;
+        }else {
+            wasRunning = running;
+            running = true;
+        }
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasRunning){
+            running = true;
+        }
+    }
+
+
+
+    //do nothing on back pressed
+    @Override
+    public void onBackPressed(){}
+    public void onClickStart(View view){
+        running = true;
+    }
+    public void onClickPause(View view){
+        running = false;
+    }
+    public void onClickResume(View view){
+        running = true;
+    }
+    public  void onClickComplete(View view){
+        running = false;
+        seconds = 0;
+    }
+    private void runTimer(){
+        final Handler handler = new Handler();
+        // Call the post() method, passing in a new Runnable.
+        // The post() method processes code without a delay,so the code in the Runnable will run almost immediately.
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                stopWatch = String.format(HouseOfCommons.locale,"%d:%02d:%02d",hours, minutes, secs);
+                lostTime.setText(stopWatch);
+
+                if (running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem){
+        if (timeTrackerActionBarDrawerToggle.onOptionsItemSelected(menuItem)){
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_tracker);
+
+
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Time tracker");
+
+        timeTrackerDrawerLayout = findViewById(R.id.timeTrackerDrawerLayout);
+
+        // pass the Open and Close toggle for the drawer layout listener to toggle the button
+        timeTrackerActionBarDrawerToggle = new ActionBarDrawerToggle(this,timeTrackerDrawerLayout,R.string.open_drawer,R.string.close_drawer);
+
+        timeTrackerDrawerLayout.addDrawerListener(timeTrackerActionBarDrawerToggle);
+        timeTrackerActionBarDrawerToggle.syncState();
+
+
+        timeTrackerNavigationView = findViewById(R.id.timeTrackerNavigationView);
+        timeTrackerNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_account:
+                        startActivity(new Intent(getApplicationContext(), editAccount.class));
+                        break;
+                    case R.id.nav_dashboard:
+                        startActivity(new Intent(getApplicationContext(),Dashboard.class));
+                        break;
+                    case R.id.nav_timeTracker:
+                        startActivity(new Intent(getApplicationContext(),timeTracker.class));
+                        break;
+                    case R.id.nav_addTasks:
+                        startActivity(new Intent(getApplicationContext(),AddTasks.class));
+                        break;
+                    case R.id.nav_pendingTasks:
+                        startActivity(new Intent(getApplicationContext(), tasks.class));
+                        break;
+                    case R.id.nav_completedTasks:
+                        startActivity(new Intent(getApplicationContext(), completed.class));
+                        break;
+                    case R.id.nav_draftTasks:
+                        startActivity(new Intent(getApplicationContext(),DraftTasks.class));
+                        break;
+                    case R.id.nav_security:
+                        startActivity(new Intent(getApplicationContext(), inHousePasswordReset.class));
+                        break;
+                    case R.id.nav_logout:
+                        SharedPreferences prf;
+                        prf = getSharedPreferences("user_details",MODE_PRIVATE);
+                        Intent i = new Intent(getApplicationContext(),Login.class);
+                        SharedPreferences.Editor editor = prf.edit();
+                        editor.clear();
+                        editor.apply();
+                        startActivity(i);
+                        break;
+
+                }
+                return false;
+            }
+        });
+
+        //restore saved instance when page is resumed
+        if (savedInstanceState != null){
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
 
         dbHelper = new DBHelper(this);
 
@@ -42,6 +203,7 @@ SharedPreferences spf;
 
 
         lostTime = findViewById(R.id.lostTime);
+        previousLostTime = findViewById(R.id.previousLostTime);
 
         startTimer = findViewById(R.id.startTimer);
         pauseTimer = findViewById(R.id.pauseTimer);
@@ -53,10 +215,9 @@ SharedPreferences spf;
         completeTimer.setVisibility(View.INVISIBLE);
 
         startTimer.setOnClickListener(view -> {
-
+            onClickStart(view);
             state = 0;
             startTimerTime = new Date().getTime();
-            lostTime.setText(R.string.timer_loading);
 
             startTimer.setVisibility(View.INVISIBLE);
             pauseTimer.setVisibility(View.VISIBLE);
@@ -65,14 +226,20 @@ SharedPreferences spf;
         });
 
         pauseTimer.setOnClickListener(view -> {
+            onClickPause(view);
             state = 1;
             pauseTimerTime = new Date().getTime();
             startTimer.setVisibility(View.INVISIBLE);
             pauseTimer.setVisibility(View.INVISIBLE);
             resumeTimer.setVisibility(View.VISIBLE);
             completeTimer.setVisibility(View.VISIBLE);
+
+            duration = (pauseTimerTime - startTimerTime);
+
+            lostTime.setText(HouseOfCommons.returnDuration(duration));
         });
         resumeTimer.setOnClickListener(view -> {
+            onClickResume(view);
             state = 2;
             resumeTimerTime = new Date().getTime();
             startTimer.setVisibility(View.INVISIBLE);
@@ -82,6 +249,7 @@ SharedPreferences spf;
         });
         completeTimer.setOnClickListener(view -> {
 
+            onClickComplete(view);
 
             completeTimerTime = new Date().getTime();
             startTimer.setVisibility(View.VISIBLE);
@@ -119,12 +287,14 @@ SharedPreferences spf;
                 switch (state){
                     case 0:
                         duration = completeTimerTime - startTimerTime;
+                        state = 3;
                         break;
                     case 2:
                         duration = (pauseTimerTime - startTimerTime) + (completeTimerTime - resumeTimerTime);
+                        state = 3;
                         break;
                 }
-                lostTime.setText(HouseOfCommons.returnDuration(duration));
+                previousLostTime.setText(String.format((HouseOfCommons.locale),"%s%s","Previous lost time: ",HouseOfCommons.returnDuration(duration)));
 
                 //db hit
                 insertTimeTrackerDetails(randUserId,startTimerTime,pauseTimerTime,resumeTimerTime,completeTimerTime,duration,reason);
@@ -137,14 +307,13 @@ SharedPreferences spf;
             alertDialog.show();
 
 
-            state = 3;
 
         });
     }
     private void insertTimeTrackerDetails(Double randUserId,long startTimerTime,long pauseTimerTime,long resumeTimerTime,long completeTimerTime,long duration,String reason){
         boolean b = dbHelper.insertIntoTimeTracker(randUserId,startTimerTime,pauseTimerTime,resumeTimerTime,completeTimerTime,duration,reason);
         if(b){
-            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Success" + duration , Toast.LENGTH_SHORT).show();
         }else{
             Toast.makeText(this, "Nuh", Toast.LENGTH_SHORT).show();
         }
